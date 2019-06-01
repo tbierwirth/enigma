5a38b1cd6e7401a1583185875a13f729eb3ca3bc
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-06-01 13:21:44 -0600

    Add generate_shift method

--- lib/offset.rb
+++ lib/offset.rb
@@ -6,8 +6,14 @@ class Offset
     Date.today.strftime("%d%m%y")
   end
 
-  def generate_offset
-    (date.to_i ** 2).to_s.split(//).last(4).join
+  def generate_offset(date)
+    (date.to_i ** 2).to_s.split(//).last(4)
+  end
+
+  def generate_shift(keys, offsets)
+    keys.map.with_index do |key, index|
+      key.to_i + offsets[index].to_i
+    end
   end
 
 end
