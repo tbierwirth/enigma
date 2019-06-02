867231ff7211db898578518131df179f19f648eb
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-06-02 17:09:32 -0600

    Initialize the class and add method encrypt a message

---
+++ lib/enigma.rb
@@ -0,0 +1,24 @@
+require './lib/keygen'
+require './lib/offset'
+require './lib/rotate'
+require 'pry'
+
+class Enigma
+
+  def initialize()
+    @keygen = KeyGen.new
+    @offset = Offset.new
+    @rotate = Rotate.new
+  end
+
+  def encrypt(message, key, date)
+    encrypted_message = []
+    shift = @offset.generate_shift(@keygen.generate_shift(key.chars), @offset.generate_offset(date))
+    message.chars.map.with_index do |char, index|
+      encrypted_message << @rotate.shift_character(char, shift.first)
+      shift.rotate!
+    end
+    encrypted_message.join
+  end
+
+end
