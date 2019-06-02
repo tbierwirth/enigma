11caf4325d7e8af96a74a9a2a3dfe70277c54bdc
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-06-02 17:09:04 -0600

    Add require for enigma, test it exists and test for encrypting a message

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -6,6 +6,7 @@ require 'minitest/pride'
 require './lib/keygen'
 require './lib/offset'
 require './lib/rotate'
+require './lib/enigma'
 
 class EnigmaTest < Minitest::Test
 
@@ -13,12 +14,18 @@ class EnigmaTest < Minitest::Test
     @keygen = KeyGen.new
     @offset = Offset.new
     @rotate = Rotate.new
+    @enigma = Enigma.new
   end
 
   def test_things_exists
     assert_instance_of KeyGen, @keygen
     assert_instance_of Offset, @offset
     assert_instance_of Rotate, @rotate
+    assert_instance_of Enigma, @enigma
+  end
+
+  def test_encrypt_message
+    assert_equal "keder ohulw", @enigma.encrypt("Hello world", "02715", "040895")
   end
 
 end
