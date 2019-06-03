7f8eeaa0ae3e744acb727db34c22d48db38c83ac
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-06-03 10:05:36 -0600

    Add method to write the decrypted text to a text file

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -4,4 +4,8 @@ enigma = Enigma.new
 encrypted = File.open(ARGV[0], &:readline)
 decrypted_message = enigma.decrypt(encrypted, ARGV[2], ARGV[3])
 
+decrypted = File.open(ARGV[1], "w")
+decrypted.write(decrypted_message[:decryption])
+
+puts "Created '#{ARGV[1]}' with the key #{decrypted_message[:key]} and date #{decrypted_message[:date]}"
 decrypted.close
