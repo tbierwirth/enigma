71828d56d5937f946dcab4df75b3701495734bc6
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-06-02 21:40:36 -0600

    Add method to decrypt a message

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -21,4 +21,14 @@ class Enigma
     hash = {encryption: encrypted_message.join, key: key, date: date}
   end
 
+  def decrypt(message, key, date = @offset.date)
+    decrypted_message = []
+    shift = @offset.generate_shift(@keygen.generate_shift(key.chars), @offset.generate_offset(date))
+    message.chars.map.with_index do |char, index|
+      decrypted_message << @rotate.shift_character(char, shift.first, unshift = true)
+      shift.rotate!
+    end
+    hash = {decryption: decrypted_message.join, key: key, date: date}
+  end
+
 end
