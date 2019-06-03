189aa3ab5bb71c15922ec295da28fcedea7466e8
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-06-02 18:32:41 -0600

    Fix generate shift method if key is being generate

--- lib/keygen.rb
+++ lib/keygen.rb
@@ -2,11 +2,16 @@ class KeyGen
 
   def key
     key = 5.times.map{rand(0..9)}
+    key.join
   end
 
   def generate_shift(key)
     shift = []
-    key.each_cons(2){|a| shift << a.join}
+    if key.class == Array
+      key.each_cons(2){|a| shift << a.join}
+    else
+      key.chars.each_cons(2){|a| shift << a.join}
+    end
     shift
   end
 
