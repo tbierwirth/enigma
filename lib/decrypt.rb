f574c63de323680a32c4842930e866ef3f9cea6a
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-06-03 13:30:14 -0600

    Add conditional to check if exactly four parameters were given

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -1,6 +1,12 @@
 require './lib/enigma'
 enigma = Enigma.new
 
+if ARGV.length != 4
+    puts "Please give exactly four parameters."
+    puts "Example: ruby ./lib/decrypt.rb encrypted.txt decrypted.txt 82648 240818"
+    exit
+end
+
 encrypted = File.open(ARGV[0], &:readline)
 decrypted_message = enigma.decrypt(encrypted, ARGV[2], ARGV[3])
 
